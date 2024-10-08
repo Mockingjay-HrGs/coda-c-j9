29af459b242c596f7446435a54ef0fc6df21f6d9
Mockingjay_HrGs <noah.dunis@coda-student.school>
2024-10-08 16:11:44 +0200

    chaud

---
+++ struct.h
@@ -0,0 +1,19 @@
+#ifndef struct_h
+#define struct_h
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+struct s_char {
+    
+    int strength;
+    int intelligence;
+    int wisdom;
+    int agility;
+    int endurance;
+};
+
+typedef struct s_char character;
+
+#endif
